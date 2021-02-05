6eda89638663d3170ada3b7de23365d7d0fd78e4
Jenkinsser <yagizbozbas@gmail.com>
2021-02-05 22:52:52 +0300

    kazanma

--- ui/src/app/services/game.service.ts
+++ ui/src/app/services/game.service.ts
@@ -13,6 +13,8 @@ export class GameService {
     ['', '', ''],
   ]
 
+  tour : number = 0;
+
   constructor(
     private readonly socket: Socket
   ) {
@@ -23,6 +25,43 @@ export class GameService {
 
   onSquareClick(rowIndex: number, squareIndex: number) {
     console.log(rowIndex, squareIndex)
+    if(this.tour%2==0){
+      if(this.board[rowIndex][squareIndex]==''){
+        this.board[rowIndex][squareIndex]='X';
+        this.tour++;
+      }
+    }
+    else{
+      if(this.board[rowIndex][squareIndex]==''){
+        this.board[rowIndex][squareIndex]='O';
+        this.tour++;
+      }
+    }
+    for(let i=0;i<3;i++){
+      if(this.board[i][0]=="X"&&this.board[i][0]==this.board[i][1]&&this.board[i][0]==this.board[i][2]){
+        console.log("1.oyuncu kazandi!");
+      }
+      if(this.board[i][0]=="O"&&this.board[i][0]==this.board[i][1]&&this.board[i][0]==this.board[i][2]){
+        console.log("2.oyuncu kazandi!");
+      }
+      if(this.board[0][i]=="X"&&this.board[0][i]==this.board[1][i]&&this.board[0][i]==this.board[2][i]){
+        console.log("1.oyuncu kazandi!");
+      }
+      if(this.board[0][i]=="O"&&this.board[0][i]==this.board[1][i]&&this.board[0][i]==this.board[2][i]){
+        console.log("2.oyuncu kazandi!");
+      }
+    }
+    if(this.board[0][0]=="X"&&this.board[0][0]==this.board[1][1]&&this.board[0][0]==this.board[2][2]){
+      console.log("1.oyuncu kazandi!");
+    }
+    if(this.board[0][0]=="O"&&this.board[0][0]==this.board[1][1]&&this.board[0][0]==this.board[2][2]){
+      console.log("2.oyuncu kazandi!");
+    }
+    if(this.board[0][2]=="X"&&this.board[0][2]==this.board[1][1]&&this.board[0][2]==this.board[2][0]){
+      console.log("1.oyuncu kazandi!");
+    }
+    if(this.board[0][2]=="O"&&this.board[0][2]==this.board[1][1]&&this.board[0][2]==this.board[2][0]){
+      console.log("2.oyuncu kazandi!");
+    }
   }
-
 }
