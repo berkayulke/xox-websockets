cb18712e62dcce31ff7394e8be6b6d47bce99213
Jenkinsser <yagizbozbas@gmail.com>
2021-02-05 22:58:29 +0300

    oyunu bitir

--- ui/src/app/services/game.service.ts
+++ ui/src/app/services/game.service.ts
@@ -15,6 +15,8 @@ export class GameService {
 
   tour : number = 0;
 
+  flag : boolean = false;
+
   constructor(
     private readonly socket: Socket
   ) {
@@ -24,6 +26,9 @@ export class GameService {
   }
 
   onSquareClick(rowIndex: number, squareIndex: number) {
+    if(this.flag){
+      return;
+    }
     console.log(rowIndex, squareIndex)
     if(this.tour%2==0){
       if(this.board[rowIndex][squareIndex]==''){
@@ -40,28 +45,36 @@ export class GameService {
     for(let i=0;i<3;i++){
       if(this.board[i][0]=="X"&&this.board[i][0]==this.board[i][1]&&this.board[i][0]==this.board[i][2]){
         console.log("1.oyuncu kazandi!");
+        this.flag=true;
       }
       if(this.board[i][0]=="O"&&this.board[i][0]==this.board[i][1]&&this.board[i][0]==this.board[i][2]){
         console.log("2.oyuncu kazandi!");
+        this.flag=true;
       }
       if(this.board[0][i]=="X"&&this.board[0][i]==this.board[1][i]&&this.board[0][i]==this.board[2][i]){
         console.log("1.oyuncu kazandi!");
+        this.flag=true;
       }
       if(this.board[0][i]=="O"&&this.board[0][i]==this.board[1][i]&&this.board[0][i]==this.board[2][i]){
         console.log("2.oyuncu kazandi!");
+        this.flag=true;
       }
     }
     if(this.board[0][0]=="X"&&this.board[0][0]==this.board[1][1]&&this.board[0][0]==this.board[2][2]){
       console.log("1.oyuncu kazandi!");
+      this.flag=true;
     }
     if(this.board[0][0]=="O"&&this.board[0][0]==this.board[1][1]&&this.board[0][0]==this.board[2][2]){
       console.log("2.oyuncu kazandi!");
+      this.flag=true;
     }
     if(this.board[0][2]=="X"&&this.board[0][2]==this.board[1][1]&&this.board[0][2]==this.board[2][0]){
       console.log("1.oyuncu kazandi!");
+      this.flag=true;
     }
     if(this.board[0][2]=="O"&&this.board[0][2]==this.board[1][1]&&this.board[0][2]==this.board[2][0]){
       console.log("2.oyuncu kazandi!");
+      this.flag=true;
     }
   }
 }
