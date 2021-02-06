19819672f2bcaba470f66c47d0d3ab09ca0e8675
Berkay Ülke <berkayulke99@gmail.com>
2021-02-06 11:50:57 +0300

    replaced Array<> type with []

--- shared/game.types.ts
+++ shared/game.types.ts
@@ -1,4 +1,4 @@
 export type BoardSquare = 'X' | 'O' | ''
 export type Player = 'X' | 'O'
-export type BoardRow = Array<BoardSquare>
-export type Board = Array<BoardRow>
+export type BoardRow = BoardSquare[]
+export type Board = BoardRow[]
